a90d2096364759d181e7d6e4f4634bad51b33122
Leszek Czechak <leszekczechak@gmail.com>
2020-09-24 22:59:18 +0200

    JwtUtil added

--- src/main/java/com/czechak/leszek/SecurityLesson/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/czechak/leszek/SecurityLesson/configuration/security/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package com.czechak.leszek.SecurityLesson.configuration.security;
 
+import com.czechak.leszek.SecurityLesson.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,5 +35,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+
     }
 }
